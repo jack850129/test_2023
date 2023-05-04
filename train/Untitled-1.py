7e6edec28b816f99375d18109adda2370ad7acf6
JACK <ccosf850321@gmail.com>
2023-05-04 16:37:45 +0800

    14

--- train/Untitled-1.py
+++ train/Untitled-1.py
@@ -3,6 +3,7 @@ def longestCommonPrefix(self, v) -> str:
     v=sorted(v)
     first=v[0]
     last=v[-1]
+    print(v,first,last)
     for i in range(min(len(first),len(last))):
         if(first[i]!=last[i]):
             return ans
