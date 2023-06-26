20371a4af4832e9a01b4b1d35733a8b2f14e8ca5
JACK <ccosf850321@gmail.com>
2023-06-26 17:21:29 +0800

    123

--- train/test.py
+++ train/test.py
@@ -1,1 +1,29 @@
-print('hello')
+# add Microsoft Teams Incoming Webhook first
+
+import pymsteams
+
+
+
+# you must create the connectorcard object with the Microsoft Webhook URL
+
+myTeamsMessage = pymsteams.connectorcard("")
+
+
+
+myTeamsMessage.title("This is my message title")
+
+myTeamsMessage.text("this is my text")
+
+myTeamsMessage.addLinkButton("This is the button Text", "https://github.com/rveachkc/pymsteams/")
+
+
+
+# preview your object
+
+# myTeamsMessage.printme()
+
+
+
+# send the message
+
+myTeamsMessage.send()
