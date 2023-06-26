00f0638b833e60c98c23a66525a36691d076da28
JACK <ccosf850321@gmail.com>
2023-06-26 17:21:43 +0800

    123

--- train/test.py
+++ train/test.py
@@ -6,7 +6,7 @@ import pymsteams
 
 # you must create the connectorcard object with the Microsoft Webhook URL
 
-myTeamsMessage = pymsteams.connectorcard("")
+myTeamsMessage = pymsteams.connectorcard("https://tejcomtw.webhook.office.com/webhookb2/9d4d4036-fa12-4c6a-bd26-5da955301e40@c3654575-c406-43b3-9dfa-d367e3435661/IncomingWebhook/e5c7235fc3864e5a971fff30a1d51069/e7a99f51-fa98-4f71-bd56-be6932556da9")
 
 
 
